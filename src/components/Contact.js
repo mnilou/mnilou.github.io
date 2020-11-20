4c85c6e5d82f70fbe05288f06192e32e75bdcd14
Nilou Mostofi <nilou@km-direct.com>
2020-11-20 00:09:30 -0800

    mailto button

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,61 +1,63 @@
-import React from "react";
-import { makeStyles, withStyles } from "@material-ui/core/styles";
-import Box from "@material-ui/core/Box";
-import TextField from "@material-ui/core/TextField";
-import Typography from "@material-ui/core/Typography";
-import Grid from "@material-ui/core/Grid";
-import Button from "@material-ui/core/Button";
+import React from 'react';
+import {makeStyles, withStyles} from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+// import Button from "@material-ui/core/Button";
+import Mailto from 'react-mailto.js';
 
-import Send from "@material-ui/icons/Send";
+import Send from '@material-ui/icons/Send';
 
 const useStyles = makeStyles((theme) => ({
   contactContainer: {
-    background: "DarkSlateGray",
-    height: "100vh",
+    background: 'DarkSlateGray',
+    height: '100vh',
   },
   heading: {
-    color: "Aquamarine",
-    textAlign: "center",
-    textTransform: "uppercase",
-    marginBottom: "1rem",
+    color: 'Aquamarine',
+    textAlign: 'center',
+    textTransform: 'uppercase',
+    marginBottom: '1rem',
   },
   form: {
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    position: "absolute",
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    position: 'absolute',
   },
   input: {
-    color: "#fff",
+    color: '#fff',
   },
   button: {
-    marginTop: "1rem",
-    color: "Aquamarine",
-    borderColor: "tan",
+    marginTop: '1rem',
+    textTransform: 'uppercase',
+    color: 'Aquamarine',
+    borderColor: 'tan',
   },
   field: {
-    margin: "1rem 0rem",
+    margin: '1rem 0rem',
   },
 }));
 
 const InputField = withStyles({
   root: {
-    "& label.Mui-focused": {
-      color: "Aquamarine",
+    '& label.Mui-focused': {
+      color: 'Aquamarine',
     },
-    "& label": {
-      color: "tan",
+    '& label': {
+      color: 'tan',
     },
-    "& .MuiOutlinedInput-root": {
-      "& fieldset": {
-        borderColor: "tan",
+    '& .MuiOutlinedInput-root': {
+      '& fieldset': {
+        borderColor: 'tan',
       },
-      "&:hover fieldset": {
-        borderColor: "tan",
+      '&:hover fieldset': {
+        borderColor: 'tan',
       },
-      "&.Mui-focused fieldset": {
-        color: "#fff",
-        borderColor: "tan",
+      '&.Mui-focused fieldset': {
+        color: '#fff',
+        borderColor: 'tan',
       },
     },
   },
@@ -74,13 +76,13 @@ const Contact = () => {
             fullWidth={true}
             label="Name"
             variant="outlined"
-            inputProps={{ className: classes.input }}
+            inputProps={{className: classes.input}}
           />
           <InputField
             fullWidth={true}
             label="Email"
             variant="outlined"
-            inputProps={{ className: classes.input }}
+            inputProps={{className: classes.input}}
             className={classes.field}
           />
           <InputField
@@ -89,16 +91,28 @@ const Contact = () => {
             variant="outlined"
             multiline
             rows={4}
-            inputProps={{ className: classes.input }}
+            inputProps={{className: classes.input}}
           />
-          <Button
+          <Mailto
+            secure={true}
+            className={classes.button}
+            to="nilou@km-direct.com"
+            subject="Some email subject"
+            body={['This is the body of your message', 'Multilined also'].join(
+              '\n'
+            )}
+          >
+            Contact Me
+          </Mailto>
+          {/* <Mailto
             variant="outlined"
             fullWidth={true}
+            href={"mailto: nilou@km-direct.com" + this.props.email}
             endIcon={<Send />}
             className={classes.button}
           >
             Contact Me
-          </Button>
+          </Mailto> */}
         </Box>
       </Grid>
     </Box>
